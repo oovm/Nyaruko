c131f8e4d5b36a7aaf2d7e465c2f6d2e7d12dd5d
Aster <galaster@foxmail.com>
2019-04-16 01:43:25 +0800

    ⏰ nyar-tmlang publish to npm

--- projects/nyar-tmlang/source/index.ts
+++ projects/nyar-tmlang/source/index.ts
@@ -1,1 +1,10 @@
-export * from './syntax'
+import { Syntax } from './syntax'
+import { functions, operators, keywords } from './tokens'
+
+const Tokens = {
+    Functions: [].concat.apply([], Object.values(functions)),
+    operators: [].concat.apply([], Object.values(operators)),
+    Keywords: [].concat.apply([], Object.values(keywords)),
+}
+
+export { Syntax, Tokens }
